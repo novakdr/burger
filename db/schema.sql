4d5117b84eb007459d9ecf6e0885e5b2f1f5d7d6
novakdr <novakdr23@gmail.com>
2018-01-23 01:22:40 -0800

    Fixed seeds.sql
    Fixed a wee typo with the seeds.

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE burgers_db
 USE burgers_db
 
-CREATE TABLE burgers {
+CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
     devoured BOOLEAN NOT NULL,
     PRIMARY KEY (id)
-}
+)

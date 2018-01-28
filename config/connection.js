c2368de0dd9ba3a030785fbc28d3763adecb86b6
novakdr <novakdr23@gmail.com>
2018-01-27 20:20:11 -0800

    fixed heroku connection

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,19 @@
 // DEPENDENCIES
 const mysql = require('mysql');
+const connection;
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: '8889',
-    database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        port: '8889',
+        database: 'burgers_db'
+    });
+}
 
-connection.connect((err) => {
-    if (err) {
-        console.log('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;

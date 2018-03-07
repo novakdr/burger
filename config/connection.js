df5e11ca76684bc5a0b8ef63027246efe73633db
novakdr <novakdr23@gmail.com>
2018-03-07 00:31:46 -0800

    mores

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 // DEPENDENCIES
 const mysql = require('mysql');
-var connection;
+let connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'root',
-        port: '8889',
-        database: 'burgers_db'
+        database: 'burgers_db',
+        port: '8889'
     });
 }
 
-connection.connect();
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
 module.exports = connection;

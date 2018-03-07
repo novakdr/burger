6aa8f483793a016e6b02173813ef229c92d17ebe
novakdr <novakdr23@gmail.com>
2018-03-06 23:47:35 -0800

    JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 // DEPENDENCIES
 const mysql = require('mysql');
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    port: '8889',
-    database: 'burgers_db'
-});
+const connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
